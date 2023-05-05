acd13cd6e5a7e770a51f04bda57f206cc640036e
Azul De Pol <azul.depol@gmail.com>
2023-05-05 21:03:28 +0100

    maquete los botones modales del login para el navbar con id para la logica de ventana modal con JS

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/* ● Login: diseñar una página o ventana modal desde donde un usuario administrador deberá poder
+ingresar sesión. (Este mismo login servirá para usuarios registrados en caso de realizar el
+requerimiento optativo)*/
+
