53b8a8b8c03136b1ee98bab0ad503f8e74e0923c
Azul De Pol <azul.depol@gmail.com>
2023-05-10 21:34:33 +0100

    empeze a agregar la logica del login y el logout y la funcion de verificaciones

--- js/login.js
+++ js/login.js
@@ -5,9 +5,11 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#pass");
 const alert = document.querySelector("#alert");
 
+// manejadores de eventos
 btnLogin.addEventListener("click", mostrarModalLogin);
 formularioLogin.addEventListener("submit", login);
 
+// admin hardcodeado en el localStorage
 const usuarioAdmin = {
     email: "admin@cmsaludactiva.com",
     password: "12345678Aa!",
@@ -16,7 +18,20 @@ const usuarioAdmin = {
 verificarUser();
 
 function verificarUser(){
-
+let existeUsuario = sessionStorage.getItem("user");
+if (existeUsuario) {
+    btnLogin.innerHTML = "Logout";
+    // poner cartel con display none que no muestre el boton admin si es un paciente
+}else {
+    btnLogin.innerHTML = "Login";
+    let webAdmin = window.location.origin + "/pages/administrador.html";
+    if (window.location.href === webAdmin){
+document.querySelector("main").innerHTML = `<h2 class="text-center">No tienes permisos suficientes para estar en esta página, sera redireccionado a la página principal.</h2>`
+    }
+    setTimeout(()=>{
+        window.location.href = window.location.origin;
+    }, 3000);
+}
 }
 
 function mostrarModalLogin(){
@@ -25,4 +40,16 @@ function mostrarModalLogin(){
     } else {
         logout();
     }
+}
+
+function login(e){
+    e.preventDefault();
+}
+
+function logout(){
+    sessionStorage.removeItem("user");
+    btnLogin.innerHTML = "Login";
+// mostrar boton 
+
+window.location.href = window.location.origin;
 }
