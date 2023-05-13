17d7262c021ca7cba6a379051a6d1acb40e2e78f
Malena de Arriba <dearribamalena@gmail.com>
2023-05-13 18:23:18 -0300

    se agregan los medicos al array y se guardan en local storage

--- js/class.js
+++ js/class.js
@@ -1,74 +1,94 @@
 export default class Medico {
-    #horario;
-    #matricula;
-    #nombre;
-    #especialidad;
-    #fotografia;
-    #precio;
-    #descripcion;
-  
-    constructor(matricula, nombre, especialidad, fotografia, horario, precio, descripcion) {
-      this.#matricula = matricula;
-      this.#nombre = nombre;
-      this.#especialidad = especialidad;
-      this.#fotografia = fotografia;
-      this.#horario = horario;
-      this.#precio = precio;
-      this.#descripcion = descripcion;
-    }
-    // Getters
-    get matricula() {
-        return this.#matricula;
-    }
-  
-    get nombre() {
-        return this.#nombre;
-    }
-  
-    get especialidad() {
-        return this.#especialidad;
-    }
-  
-    get fotografia() {
-        return this.#fotografia;
-    }
-    
-    get descripcion() {
-        return this.#descripcion;
-    }
-    
-    get horario() {
-        return this.#horario;
-    }
-    get precio() {
-        return this.#precio;
-    }
+  #horario;
+  #matricula;
+  #nombre;
+  #especialidad;
+  #fotografia;
+  #precio;
+  #descripcion;
 
-    // Setters
-    set matricula(matricula) {
-        this.#matricula = matricula;
-    }
+  constructor(
+    matricula,
+    nombre,
+    especialidad,
+    fotografia,
+    horario,
+    precio,
+    descripcion
+  ) {
+    this.#matricula = matricula;
+    this.#nombre = nombre;
+    this.#especialidad = especialidad;
+    this.#fotografia = fotografia;
+    this.#horario = horario;
+    this.#precio = precio;
+    this.#descripcion = descripcion;
+  }
+  // Getters
+  get matricula() {
+    return this.#matricula;
+  }
 
-    set nombre(nombre) {
-        this.#nombre = nombre;
-    }
-    set especialidad(especialidad) {
-        this.#especialidad = especialidad;
-    }
-    
-    set fotografia(fotografia) {
-        this.#fotografia = fotografia;
-    }
-    
-    set descripcion(descripcion) {
-        this.#descripcion = descripcion;
-    }
-    
-    set horario(horario) {
-        this.#horario = horario;
-    }
-    
-    set precio(precio) {
-        this.#precio = precio;
-    }
+  get nombre() {
+    return this.#nombre;
+  }
+
+  get especialidad() {
+    return this.#especialidad;
+  }
+
+  get fotografia() {
+    return this.#fotografia;
+  }
+
+  get descripcion() {
+    return this.#descripcion;
+  }
+
+  get horario() {
+    return this.#horario;
+  }
+  get precio() {
+    return this.#precio;
+  }
+
+  // Setters
+  set matricula(matricula) {
+    this.#matricula = matricula;
+  }
+
+  set nombre(nombre) {
+    this.#nombre = nombre;
+  }
+  set especialidad(especialidad) {
+    this.#especialidad = especialidad;
+  }
+
+  set fotografia(fotografia) {
+    this.#fotografia = fotografia;
+  }
+
+  set descripcion(descripcion) {
+    this.#descripcion = descripcion;
+  }
+
+  set horario(horario) {
+    this.#horario = horario;
+  }
+
+  set precio(precio) {
+    this.#precio = precio;
+  }
+
+  toJSON() {
+    return {
+      matricula: this.matricula,
+      nombre: this.nombre,
+      especialidad: this.especialidad,
+      fotografia: this.fotografia,
+      horario: this.horario,
+      precio: this.precio,
+      descripcion: this.descripcion,
+    };
+  }
 }
