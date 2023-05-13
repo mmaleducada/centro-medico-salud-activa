908cfe83a6db89065f5603d84f9e3050a7783627
Azul De Pol <azul.depol@gmail.com>
2023-05-13 22:10:04 +0100

    agregue la funcion de login, incluyendo la logica para agregar y sacar el boton de administrador, pero falta maquetar el boton en el navbar html para que funcione. Tambien agregue la logica para el boton admin en las funciones verificarUser y logout. Faltan agregar las funciones de validacion para que funcione el login y testearlo

--- js/login.js
+++ js/login.js
@@ -18,20 +18,22 @@ const usuarioAdmin = {
 verificarUser();
 
 function verificarUser(){
-let existeUsuario = sessionStorage.getItem("user");
-if (existeUsuario) {
-    btnLogin.innerHTML = "Logout";
-    // poner cartel con display none que no muestre el boton admin si es un paciente
-}else {
-    btnLogin.innerHTML = "Login";
-    let webAdmin = window.location.origin + "/pages/administrador.html";
+    let existeUsuario = sessionStorage.getItem("user");
+    if (existeUsuario === usuarioAdmin) {
+        btnLogin.innerHTML = "Logout";
+        // mostrar boton de administrador 
+        document.querySelector("#btnAdmin").classList.remove("d-none");
+    }else {
+        btnLogin.innerHTML = "Login";
+        let webAdmin = window.location.origin + "/pages/administrador.html";
+    // para prevenir hackeos de la pagina admin
     if (window.location.href === webAdmin){
-document.querySelector("main").innerHTML = `<h2 class="text-center">No tienes permisos suficientes para estar en esta página, sera redireccionado a la página principal.</h2>`
+        document.querySelector("main").innerHTML = `<h2 class="text-center">No tienes permisos suficientes para estar en esta página, sera redireccionado a la página principal.</h2>`
     }
     // setTimeout(()=>{
-    //     window.location.href = window.location.origin;
-    // }, 3000);
-}
+        //     window.location.href = window.location.origin;
+        // }, 3000);
+    }
 }
 
 function mostrarModalLogin(){
@@ -41,20 +43,32 @@ function mostrarModalLogin(){
         logout();
     }
 }
-
-function login(e){
-    e.preventDefault();
-
 // verificaciones
 /* expression regular - for password "Password must contain 8 characters and at least one number, one letter and one unique character such as !#$%&? "-->
  ^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$
-*/
+ */
+// sweet alert con msj de error: "usuario o contraseña incorrecto."
+
+function login(e){
+    e.preventDefault();
+if (validarEmail() && validarPassword()) {
+    if (email.value === usuarioAdmin.value && password.value === usuarioAdmin.password) {
+        alert.className = "alert alert-danger mt-3 d-none";
+        btnLogin.innerHTML = "Logout";
+        sessionStorage.setItem("user", JSON.stringify(usuarioAdmin));
+        document.querySelector("#btnAdmin").classList.remove("d-none");
+        modalLogin.hide();
+    } else {
+        alert.className = "alert alert-danger mt-3";
+    }
+}
 }
 
 function logout(){
     sessionStorage.removeItem("user");
     btnLogin.innerHTML = "Login";
-// mostrar boton 
+    // poner cartel con display none que no muestre el boton admin si es un paciente
+    document.querySelector("#btnAdmin").classList.add("d-none");
 
 window.location.href = window.location.origin;
 }
