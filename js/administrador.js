270d2536ba5fa4be3fc5164bc7f63439b2f8c1b6
Juan Pablo Alba Fernandez <juanpialba@gmail.com>
2023-05-14 00:10:37 -0300

    creamos una funcion que lo reccore al local storage para ver si tiene datos almacenados, y con los datos ontenidos creamos una nueva instancia de la clase Medico

--- js/administrador.js
+++ js/administrador.js
@@ -2,85 +2,105 @@ import Medico from "./class.js";
 import { resumenValidaciones } from "./helpers.js";
 
 //variables globales
-let formularioMedico = document.getElementById('form-medico');
-let modalMedico = new bootstrap.Modal(document.getElementById("modalMedicosAdmi"));
-let btnModalMedico = document.getElementById('boton-sumar-medico-admi');
-let matricula = document.getElementById('inputMatricula');
-let nombre = document.getElementById('inputNombreCompleto');
-let resenia = document.getElementById('inputResenia');
-let especialidad = document.getElementById('inputEspecialidad');
-let fotografia = document.getElementById('inputFotografia');
-let horario = document.getElementById("inputHorario")
-let precio = document.getElementById('inputPrecio');
-let mensajeAlerta = document.getElementById('alerta');
+let formularioMedico = document.getElementById("form-medico");
+let modalMedico = new bootstrap.Modal(
+  document.getElementById("modalMedicosAdmi")
+);
+let btnModalMedico = document.getElementById("boton-sumar-medico-admi");
+let matricula = document.getElementById("inputMatricula");
+let nombre = document.getElementById("inputNombreCompleto");
+let resenia = document.getElementById("inputResenia");
+let especialidad = document.getElementById("inputEspecialidad");
+let fotografia = document.getElementById("inputFotografia");
+let horario = document.getElementById("inputHorario");
+let precio = document.getElementById("inputPrecio");
+let mensajeAlerta = document.getElementById("alerta");
 let listaMedicos = [];
 
 //manejadores de eventos
 btnModalMedico.addEventListener("click", desplegarModalMedico);
-formularioMedico.addEventListener('submit', prepararFormularioMedico);
+formularioMedico.addEventListener("submit", prepararFormularioMedico);
 
 //leer la pelicua de el array de pelicua con setiten
-let listaMedico = JSON.parse(localStorage.getItem('listaMedicos')) || [];
+let listaMedico = JSON.parse(localStorage.getItem("listaMedicos")) || [];
+if (listaMedico.length > 0) {
+  listaMedico = listaMedico.map(
+    (medico) =>
+      new Medico(
+        medico.nombre,
+        medico.especialidad,
+        medico.fotografia,
+        medico.horario,
+        medico.precio,
+        medico.resenia
+      )
+  );
+}
+console.log(listaMedico) /// viveeee 
 
 //definir funcion carga inicial
 //definir funcion crearfila
-//corregir el numero de indice 
+//corregir el numero de indice
 
 //funciones
 function desplegarModalMedico() {
-    modalMedico.show();    
+  modalMedico.show();
 }
-function prepararFormularioMedico(e){
-    e.preventDefault();
-    crearMedico();
+function prepararFormularioMedico(e) {
+  e.preventDefault();
+  crearMedico();
 }
 
-function mostrarMensajeError (resumen) {
-    if(resumen.length > 0) {
-        mensajeAlerta.className = "alert alert-danger mt-3";
-        mensajeAlerta.innerHTML = resumen;
-    } else {
-        mensajeAlerta.className = "alert alert-danger mt-3 d-none";
-    }
+function mostrarMensajeError(resumen) {
+  if (resumen.length > 0) {
+    mensajeAlerta.className = "alert alert-danger mt-3";
+    mensajeAlerta.innerHTML = resumen;
+  } else {
+    mensajeAlerta.className = "alert alert-danger mt-3 d-none";
+  }
 }
 
 function guardarEnLocalStorage() {
-    localStorage.setItem("listaMedicos", JSON.stringify(listaMedicos));
+  localStorage.setItem("listaMedicos", JSON.stringify(listaMedicos));
 }
 
-function limpiarForm (){
-    formularioMedico.reset();
-    modalMedico.hide();
+function limpiarForm() {
+  formularioMedico.reset();
+  modalMedico.hide();
 }
 
-function crearMedico () {
-    const resumen = resumenValidaciones(nombre.value, resenia.value, especialidad.value, fotografia.value, horario.value, precio.value);
-    
-    mostrarMensajeError(resumen);
+function crearMedico() {
+  const resumen = resumenValidaciones(
+    nombre.value,
+    resenia.value,
+    especialidad.value,
+    fotografia.value,
+    horario.value,
+    precio.value
+  );
 
+  mostrarMensajeError(resumen);
 
-    if(resumen.length === 0){
-        const medicoNuevo = new Medico (
-            undefined, 
-            nombre.value,
-            especialidad.value, 
-            fotografia.value, 
-            horario.value, 
-            precio.value,
-            resenia.value); 
+  if (resumen.length === 0) {
+    const medicoNuevo = new Medico(
+      undefined,
+      nombre.value,
+      especialidad.value,
+      fotografia.value,
+      horario.value,
+      precio.value,
+      resenia.value
+    );
 
-        listaMedicos.push(medicoNuevo);
-        
-        
-        guardarEnLocalStorage();
-        Swal.fire(
-            "Carga exitosa",
-            "El medico se cargo correctamente en la lista",
-            "success"
-        );
+    listaMedicos.push(medicoNuevo);
 
-        limpiarForm();
+    guardarEnLocalStorage();
+    Swal.fire(
+      "Carga exitosa",
+      "El medico se cargo correctamente en la lista",
+      "success"
+    );
 
-    }
-    
+    limpiarForm();
+  }
 }
