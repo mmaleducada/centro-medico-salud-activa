c238a76fe75cb9cc686c803a53c28ef246e0c462
Azul De Pol <azul.depol@gmail.com>
2023-05-15 22:26:10 +0100

    arregle la variable para el boton admin. y segui testeando y trabajando las cosas que no funcionan - la funcion login y logout.

--- js/login.js
+++ js/login.js
@@ -1,13 +1,15 @@
 const modalLogin = new bootstrap.Modal(document.querySelector("#modalLogin"));
 const btnIniciarSesion = document.querySelector("#btnIniciarSesion")
 const btnLogin = document.querySelector("#btnLogin");
-const formularioLogin = document.querySelector("#formLogin");
+const btnAdmin = document.querySelector("#btnAdmin");
 const email = document.querySelector("#email");
 const password = document.querySelector("#pass");
 const alert = document.querySelector("#alert");
+const formularioLogin = document.querySelector("#formLogin");
 
 btnIniciarSesion.addEventListener("click", desplegarModalLogin);
-formularioLogin.addEventListener("submit", login);
+btnLogin.addEventListener("submit", login);
+
 const usuarioAdmin = {
     email: "admin@cmsaludactiva.com",
     password: "12345678Aa!",
@@ -18,17 +20,19 @@ verificarUser();
 
 function verificarUser(){
     let existeUsuario = sessionStorage.getItem("user");
-    if (existeUsuario === usuarioAdmin) {
-        btnIniciarSesion.innerHTML = "Salir";
-        document.querySelector("#btnAdmin").classList.remove("d-none");
+    if (existeUsuario) {
+        // btnIniciarSesion.innerHTML = "Salir";
+        // btnAdmin.remove("d-none");
+        // TODO agregar validaciones
     }else {
         btnIniciarSesion.innerHTML = "Iniciar Sesión";
         let webAdmin = window.location.origin + "/pages/administrador.html";
-    if (window.location.href === webAdmin){
-        document.querySelector("main").innerHTML = `<h2 class="text-center">No tienes permisos suficientes para estar en esta página, sera redireccionado a la página principal.</h2>`
-    }
+        if (window.location.href === webAdmin){
+            document.querySelector("main").innerHTML = `<h2 class="text-center">No tienes permisos suficientes para estar en esta página, sera redireccionado a la página principal.</h2>`
+        }
     }
 }
+
 
 function desplegarModalLogin(){
     if (btnIniciarSesion.innerHTML === "Iniciar Sesión"){
@@ -40,20 +44,23 @@ function desplegarModalLogin(){
 
 function login(e){
     e.preventDefault();
-
-    if (email.value === usuarioAdmin.value && password.value === usuarioAdmin.password) {
-        alert.className = "alert alert-danger mt-3 d-none";
-        btnIniciarSesion.innerHTML = "Salir";
-        sessionStorage.setItem("user", JSON.stringify(usuarioAdmin));
-        document.querySelector("#btnAdmin").classList.remove("d-none");
-        modalLogin.hide();
-    } else {
-    }
-}
-
-function logout(){
-    sessionStorage.removeItem("user");
-    btnIniciarSesion.innerHTML = "Iniciar Sesión";
+    // TODO descomentar la linea 47 cuando se agreguen las validaciones
+    // if (validarEmail() && validarPassword()){
+        if (email.value === usuarioAdmin.value && password.value === usuarioAdmin.password) {
+                console.log(`hola admin`);
+                btnIniciarSesion.innerHTML = "Salir";
+                btnAdmin.classList.remove("d-none");
+            modalLogin.hide();
+        } else {
+        }
+    }   
+    // }
+    
+    function logout(){
+        if (btnIniciarSesion.innerHTML = "Salir"){
+            sessionStorage.removeItem("user");
+            btnIniciarSesion.innerHTML = "Iniciar Sesión";
     document.querySelector("#btnAdmin").classList.add("d-none");
-window.location.href = window.location.origin;
+    window.location.href = window.location.origin;
+}
 }
