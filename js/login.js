c38c1da52c9e1766715d73759e240c38ad4e9c84
Azul De Pol <azul.depol@gmail.com>
2023-05-18 00:31:52 +0100

    resolvimos el problema del boton admin y entrar. Agregamos validaciones

--- js/login.js
+++ js/login.js
@@ -1,70 +1,90 @@
 const modalLogin = new bootstrap.Modal(document.querySelector("#modalLogin"));
 const btnIniciarSesion = document.querySelector("#btnIniciarSesion")
 const btnLogin = document.querySelector("#btnLogin");
-const btnAdmin = document.querySelector("#btnAdmin");
 const email = document.querySelector("#email");
 const password = document.querySelector("#pass");
-const alert = document.querySelector("#alert");
+const alert = document.querySelector("#alerta");
 const formularioLogin = document.querySelector("#formLogin");
-
-btnIniciarSesion.addEventListener("click", desplegarModalLogin);
-btnLogin.addEventListener("submit", login);
+const linkAdmin = document.querySelector("#linkAdmin");
+const expRegCorreo = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+const expRegContrasenia = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/
 
 const usuarioAdmin = {
     email: "admin@cmsaludactiva.com",
     password: "12345678Aa!",
 }
+btnIniciarSesion.addEventListener("click", desplegarModalLogin);
+formularioLogin.addEventListener("submit", login);
 
 sessionStorage.setItem("user", JSON.stringify(usuarioAdmin));
-verificarUser();
-console.log(window.location.href);
-console.log(window.location.origin);
-
-function verificarUser(){
-    let existeUsuario = sessionStorage.getItem("user");
-    if (existeUsuario) {
-        btnIniciarSesion.innerHTML = "Salir";
-        btnAdmin.classList.remove("d-none");
-       // TODO agregar validaciones
-    }else {
-        btnIniciarSesion.innerHTML = "Iniciar Sesión";
-        let webAdmin = window.location.origin+ "/pages/administrador.html";
-        if (window.location.href === webAdmin){
-            document.querySelector("main").innerHTML = `<h2 class="text-center bg-dark text-light">No tienes permisos suficientes para estar en esta página, sera redireccionado a la página principal.</h2>`
-        }
-        setTimeout( ()=>{    
-            window.location.href = window.location.origin;
-        }, 3000)
-    }
-}
 
 
 function desplegarModalLogin(){
     if (btnIniciarSesion.innerHTML === "Iniciar Sesión"){
+        formularioLogin.reset();
         modalLogin.show();
-    } else {
-        logout();
     }
 }
-
 function login(e){
     e.preventDefault();
-    // TODO descomentar las lineas 47 y 64 cuando se agreguen las validaciones (y borrar esta!)
-    // if (validarEmail() && validarPassword()){
-        if (email.value === usuarioAdmin.value && password.value === usuarioAdmin.password) {
-                console.log(`hola admin`);
-                btnIniciarSesion.innerHTML = "Salir";
-                btnAdmin.classList.remove("d-none");
-                modalLogin.hide();
-        } 
-    }   
-    // }
-    
-    function logout(){
+    if (validarEmail() && validarPassword()){
+        if (btnIniciarSesion.innerHTML === "Iniciar Sesión"){
+        verificarUser();
+        linkAdmin.classList.remove("d-none");
         modalLogin.hide();
-            sessionStorage.removeItem("user");
-            btnIniciarSesion.innerHTML = "Iniciar Sesión";
-            btnAdmin.classList.add("d-none");
-        window.location.href = window.location.origin;
-            }
-        
+    } 
+}
+}
+function verificarUser(){
+    const datosAdmin = JSON.parse(sessionStorage.getItem('user'));
+	const correoAdmin = datosAdmin.email;
+	const contraseniaAdmin = datosAdmin.password;
+    sessionStorage.setItem("activo", JSON.stringify(true));
+    
+	if (email.value === correoAdmin && password.value === contraseniaAdmin) {
+        linkAdmin.classList.remove('d-none');
+        btnIniciarSesion.innerHTML = "Salir";
+	}
+    
+//     let existeUsuario = sessionStorage.getItem("activo" === true);
+//     if (existeUsuario) {
+// }
+}
+// function logout(){
+//     modalLogin.hide();
+//         sessionStorage.removeItem("user");
+//         btnIniciarSesion.innerHTML = "Iniciar Sesión";
+//         linkAdmin.classList.add("d-none");
+//     window.location.href = window.location.origin;
+//         }
+
+
+// validaciones
+function validarEmail(){
+    const correo = email.value;
+    console.log(correo);
+    if (correo !== null) {
+        if (correo.match(expRegCorreo) === null) {
+            console.log('Usuario y/o contrasenia incorrecta');
+        };
+    };
+}
+
+
+function validarPassword(){
+    const contrasenia = password.value;
+    console.log(contrasenia);
+    
+    if (contrasenia === null) {
+        console.log("Debe ingresar una contrasenia");
+    } else if (contrasenia < 8 || contrasenia > 16){
+        console.log('La contrasenia debe tener entre 8 y 16 caracteres')
+    } else {
+        if (contrasenia.match(expRegContrasenia) === null) {
+            console.log('Usuario y/o contrasenia incorrecta');
+        } else {
+            console.log('Contrasenia invalida')
+        }
+    };
+    
+}
