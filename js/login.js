9952760582a79b86efc94279f5e78bd3c76ac2f5
Azul De Pol <azul.depol@gmail.com>
2023-05-18 00:56:18 +0100

    solucionamos todos los problemas de los botones - agregando un nuevo boton salir que aparece cuando el admin esta activo y desaparece cuando se cierra sesion

--- js/login.js
+++ js/login.js
@@ -1,13 +1,15 @@
 const modalLogin = new bootstrap.Modal(document.querySelector("#modalLogin"));
 const btnIniciarSesion = document.querySelector("#btnIniciarSesion")
 const btnLogin = document.querySelector("#btnLogin");
+const listaSalir = document.querySelector("#listaSalir");
+const btnSalir = document.querySelector("#btnSalir");
 const email = document.querySelector("#email");
 const password = document.querySelector("#pass");
 const alert = document.querySelector("#alerta");
 const formularioLogin = document.querySelector("#formLogin");
 const linkAdmin = document.querySelector("#linkAdmin");
-const expRegCorreo = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-const expRegContrasenia = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/
+// const expRegCorreo = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+// const expRegContrasenia = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/
 
 const usuarioAdmin = {
     email: "admin@cmsaludactiva.com",
@@ -15,6 +17,7 @@ const usuarioAdmin = {
 }
 btnIniciarSesion.addEventListener("click", desplegarModalLogin);
 formularioLogin.addEventListener("submit", login);
+btnSalir.addEventListener("click", logout);
 
 sessionStorage.setItem("user", JSON.stringify(usuarioAdmin));
 
@@ -27,13 +30,13 @@ function desplegarModalLogin(){
 }
 function login(e){
     e.preventDefault();
-    if (validarEmail() && validarPassword()){
+    // if (validarEmail() && validarPassword()){
         if (btnIniciarSesion.innerHTML === "Iniciar Sesión"){
-        verificarUser();
-        linkAdmin.classList.remove("d-none");
-        modalLogin.hide();
-    } 
-}
+            verificarUser();
+            linkAdmin.classList.remove("d-none");
+            modalLogin.hide();
+        } 
+        // }
 }
 function verificarUser(){
     const datosAdmin = JSON.parse(sessionStorage.getItem('user'));
@@ -42,49 +45,50 @@ function verificarUser(){
     sessionStorage.setItem("activo", JSON.stringify(true));
     
 	if (email.value === correoAdmin && password.value === contraseniaAdmin) {
-        linkAdmin.classList.remove('d-none');
-        btnIniciarSesion.innerHTML = "Salir";
+        linkAdmin.classList.remove("d-none");
+        btnIniciarSesion.classList.add("d-none");
+        listaSalir.classList.remove("d-none");
 	}
     
 //     let existeUsuario = sessionStorage.getItem("activo" === true);
 //     if (existeUsuario) {
 // }
 }
-// function logout(){
-//     modalLogin.hide();
-//         sessionStorage.removeItem("user");
-//         btnIniciarSesion.innerHTML = "Iniciar Sesión";
-//         linkAdmin.classList.add("d-none");
-//     window.location.href = window.location.origin;
-//         }
+function logout(){
+        // sessionStorage.removeItem("user");
+        btnIniciarSesion.classList.remove("d-none");
+        listaSalir.classList.add("d-none");
+        linkAdmin.classList.add("d-none");
+    }
+
 
 
 // validaciones
-function validarEmail(){
-    const correo = email.value;
-    console.log(correo);
-    if (correo !== null) {
-        if (correo.match(expRegCorreo) === null) {
-            console.log('Usuario y/o contrasenia incorrecta');
-        };
-    };
-}
+// function validarEmail(){
+//     const correo = email.value;
+//     console.log(correo);
+//     if (correo !== null) {
+//         if (correo.match(expRegCorreo) === null) {
+//             console.log('Usuario y/o contrasenia incorrecta');
+//         };
+//     };
+// }
 
 
-function validarPassword(){
-    const contrasenia = password.value;
-    console.log(contrasenia);
+// function validarPassword(){
+//     const contrasenia = password.value;
+//     console.log(contrasenia);
     
-    if (contrasenia === null) {
-        console.log("Debe ingresar una contrasenia");
-    } else if (contrasenia < 8 || contrasenia > 16){
-        console.log('La contrasenia debe tener entre 8 y 16 caracteres')
-    } else {
-        if (contrasenia.match(expRegContrasenia) === null) {
-            console.log('Usuario y/o contrasenia incorrecta');
-        } else {
-            console.log('Contrasenia invalida')
-        }
-    };
+//     if (contrasenia === null) {
+//         console.log("Debe ingresar una contrasenia");
+//     } else if (contrasenia < 8 || contrasenia > 16){
+//         console.log('La contrasenia debe tener entre 8 y 16 caracteres')
+//     } else {
+//         if (contrasenia.match(expRegContrasenia) === null) {
+//             console.log('Usuario y/o contrasenia incorrecta');
+//         } else {
+//             console.log('Contrasenia invalida')
+//         }
+//     };
     
-}
+// }
