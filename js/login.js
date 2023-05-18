5e7fd78d10bd7dc52c5ba3e13a246e5fafb4bbb5
Federico <federuedaleal@hotmail.com>
2023-05-18 01:15:40 -0300

    Login funcionando, página para sumar Login a las demás

--- js/login.js
+++ js/login.js
@@ -1,97 +1,81 @@
 const modalLogin = new bootstrap.Modal(document.querySelector("#modalLogin"));
-const btnIniciarSesion = document.querySelector("#btnIniciarSesion")
-const btnLogin = document.querySelector("#btnLogin");
-const listaSalir = document.querySelector("#listaSalir");
+const btnIniciarSesion = document.querySelector("#btnIniciarSesion");
 const btnSalir = document.querySelector("#btnSalir");
+const btnLogin = document.querySelector("#btnLogin");
 const email = document.querySelector("#email");
 const password = document.querySelector("#pass");
-const alert = document.querySelector("#alerta");
 const formularioLogin = document.querySelector("#formLogin");
-const linkAdmin = document.querySelector("#linkAdmin");
-const usuarioActivo = "";
-// const expRegCorreo = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-// const expRegContrasenia = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/
+const linkAdministrador = document.querySelector("#linkAdmin");
+
+const expRegCorreo =
+  /^[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'+/=?^_`{|}~-]+)@(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+const expRegContrasenia =
+  /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/;
 
 const usuarioAdmin = {
-    email: "admin@cmsaludactiva.com",
-    password: "12345678Aa!",
+  email: "admin@cmsaludactiva.com",
+  password: "Admin123!",
+};
+
+let admin = false;
+
+if (sessionStorage.getItem("logueado") !== null) {
+  btnIniciarSesion.classList.add("d-none");
+  btnSalir.classList.remove("d-none");
+  linkAdministrador.classList.remove("d-none");
 }
-btnIniciarSesion.addEventListener("click", desplegarModalLogin);
-formularioLogin.addEventListener("submit", login);
-btnSalir.addEventListener("click", logout);
 
 sessionStorage.setItem("user", JSON.stringify(usuarioAdmin));
-sessionStorage.setItem("activo", JSON.stringify(usuarioActivo));
 
+btnIniciarSesion.addEventListener("click", desplegarModalLogin);
+formularioLogin.addEventListener("submit", entrar);
+btnSalir.addEventListener("click", cerrarSesion);
 
-function desplegarModalLogin(){
-    if (btnIniciarSesion.innerHTML === "Iniciar Sesión"){
-        formularioLogin.reset();
-        modalLogin.show();
+function entrar(e) {
+  e.preventDefault();
+  if (btnIniciarSesion.innerHTML === "Iniciar Sesión") {
+    verificarUsuario();
+  }
+}
+
+function verificarUsuario() {
+  const datosAdmin = JSON.parse(sessionStorage.getItem("user"));
+  const correoAdmin = datosAdmin.email;
+  const contraseniaAdmin = datosAdmin.password;
+
+  if (
+    email.value.match(expRegCorreo) === null ||
+    password.value.match(expRegContrasenia) === null
+  ) {
+    Swal.fire("Correo y/o contraseña Invalido");
+    formularioLogin.reset();
+  } else {
+    if (email.value === correoAdmin && password.value === contraseniaAdmin) {
+      admin = true;
+      sessionStorage.setItem("adminLog", JSON.stringify(admin));
+      linkAdministrador.classList.remove("d-none");
+      btnSalir.classList.remove("d-none");
+      btnIniciarSesion.classList.add("d-none");
+      modalLogin.hide();
+    } else {
+      Swal.fire("Ingreso usuario normal");
+      modalLogin.hide();
+      btnIniciarSesion.classList.add("d-none");
+      btnSalir.classList.remove("d-none");
     }
+  }
 }
-function login(e){
-    e.preventDefault();
-    // if (validarEmail() && validarPassword()){
-        if (btnIniciarSesion.innerHTML === "Iniciar Sesión"){
-            verificarUser();
-            linkAdmin.classList.remove("d-none");
-            modalLogin.hide();
-        } 
-        // }
+
+function cerrarSesion() {
+  sessionStorage = false;
+  sessionStorage.removeItem("adminLog");
+  linkAdministrador.classList.add("d-none");
+  btnSalir.classList.add("d-none");
+  btnIniciarSesion.classList.remove("d-none");
 }
-function verificarUser(){
-    sessionStorage.setItem("activo", JSON.stringify(true));
-    const datosAdmin = JSON.parse(sessionStorage.getItem("user"));
-	const correoAdmin = datosAdmin.email;
-	const contraseniaAdmin = datosAdmin.password;
-    
-	if (email.value === correoAdmin && password.value === contraseniaAdmin) {
-        linkAdmin.classList.remove("d-none");
-        btnIniciarSesion.classList.add("d-none");
-        listaSalir.classList.remove("d-none");
-	}
-    
-    let existeUsuario = sessionStorage.getItem("activo", (true));
-    console.log(existeUsuario);
-//     if (existeUsuario) {
-// }
+
+function desplegarModalLogin() {
+  if (btnIniciarSesion.innerHTML === "Iniciar Sesión") {
+    formularioLogin.reset();
+  }
 }
-function logout(){
-    sessionStorage.removeItem("activo");
-        btnIniciarSesion.classList.remove("d-none");
-        listaSalir.classList.add("d-none");
-        linkAdmin.classList.add("d-none");
-    }
-
-
-
-// validaciones
-// function validarEmail(){
-//     const correo = email.value;
-//     console.log(correo);
-//     if (correo !== null) {
-//         if (correo.match(expRegCorreo) === null) {
-//             console.log('Usuario y/o contrasenia incorrecta');
-//         };
-//     };
-// }
-
-
-// function validarPassword(){
-//     const contrasenia = password.value;
-//     console.log(contrasenia);
-    
-//     if (contrasenia === null) {
-//         console.log("Debe ingresar una contrasenia");
-//     } else if (contrasenia < 8 || contrasenia > 16){
-//         console.log('La contrasenia debe tener entre 8 y 16 caracteres')
-//     } else {
-//         if (contrasenia.match(expRegContrasenia) === null) {
-//             console.log('Usuario y/o contrasenia incorrecta');
-//         } else {
-//             console.log('Contrasenia invalida')
-//         }
-//     };
-    
-// }
